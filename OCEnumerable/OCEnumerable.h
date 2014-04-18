917c6ebe6b59ee7a6006909149d1515f7b5a413e
Zen Kyprianou <zen@kyprianou.eu>
2014-04-18 23:15:38 +0100

    Install xcode project

---
+++ OCEnumerable/OCEnumerable.h
@@ -0,0 +1,13 @@
+//
+//  OCEnumerable.h
+//  OCEnumerable
+//
+//  Created by Zinon Kyprianou on 18/04/2014.
+//  Copyright (c) 2014 Zen Kyprianou. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OCEnumerable : NSObject
+
+@end
---
+++ OCEnumerable/OCEnumerable.m
@@ -0,0 +1,13 @@
+//
+//  OCEnumerable.m
+//  OCEnumerable
+//
+//  Created by Zinon Kyprianou on 18/04/2014.
+//  Copyright (c) 2014 Zen Kyprianou. All rights reserved.
+//
+
+#import "OCEnumerable.h"
+
+@implementation OCEnumerable
+
+@end
